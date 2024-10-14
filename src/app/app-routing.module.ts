b4f465ed11ed13e65e1a522d933038e357962bf8
Abdallah <bratosab@gmail.com>
2024-10-14 14:26:32 +0200

    feat(kitchen): added lazy loaded module kitchen

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'kitchen',
+    loadChildren: () =>
+      import('./kitchen/kitchen.module').then((m) => m.KitchenModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
