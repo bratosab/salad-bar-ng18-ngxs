b0403432533415156b1b58c8b2ade5c6ad2f891a
Abdallah <bratosab@gmail.com>
2024-10-14 16:00:18 +0200

    feat(material modules): added material 3 with shared material modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { SaladModule } from './salad/salad.module';
+import { SharedMaterialModule } from './shared-material.module';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,8 @@ import { SaladModule } from './salad/salad.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SaladModule
+    SaladModule,
+    SharedMaterialModule
   ],
   providers: [
     provideAnimationsAsync()
